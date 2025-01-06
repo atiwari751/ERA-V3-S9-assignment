5479df9e78fe40f9927e22cd79aa3289f58a0e04
ubuntu <>
2025-01-06 19:50:37 +0000

    jitter probablity changed to 0.5

--- data_utils.py
+++ data_utils.py
@@ -8,7 +8,7 @@ def get_train_transform():
     return A.Compose([
         A.RandomResizedCrop(height=224, width=224, scale=(0.08, 1.0), ratio=(3/4, 4/3), p=1.0),
         A.HorizontalFlip(p=0.5),
-        A.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.05, p=0.8),
+        A.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.05, p=0.5),
         A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
         ToTensorV2()
     ])
